08d11c2536b3b11ae6bfb57b1421a2b19198b44e
Matheus Leal <theusleals@outlook.com>
2023-12-10 01:25:43 +0000

    Criando servidor web

---
+++ Arquitetura_Exagonal/adapters/web/server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/MatheusLeal/go-hexagonal/application"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	server := &http.Server{
+		Addr:              ":8080",
+		WriteTimeout:      10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
