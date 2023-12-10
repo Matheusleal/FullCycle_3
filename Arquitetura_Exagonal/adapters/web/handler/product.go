9f1ec72dd60fda2c61083e3034a7ad086dae80dc
Matheus Leal <theusleals@outlook.com>
2023-12-10 18:10:32 +0000

    Trabalhando com DTO

--- Arquitetura_Exagonal/adapters/web/handler/product.go
+++ Arquitetura_Exagonal/adapters/web/handler/product.go
@@ -37,3 +37,12 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 		}
 	})
 }
+
+func CreateProduct(service application.ProductServiceInterface) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		
+	})
+}
