a81c272bf427a128bd1fb53a4550b8228f9bbaac
Matheus Leal <theusleals@outlook.com>
2023-12-12 18:55:39 +0000

    Implementando testes de criar customer com dados inválidos

--- Clean_Architecture/src/use_case/customer/create/create.customer.unit.spec.ts
+++ Clean_Architecture/src/use_case/customer/create/create.customer.unit.spec.ts
@@ -40,4 +40,54 @@ describe('Unit Test create Customer Use Case', () => {
 
     expect(output).toEqual(ExpectedOutput)
   })
+
+  it('should throw an error when customer name is missing', async () => {
+    const customerRepository = MockRepository()
+    const useCase = new CreateCustomerUseCase(customerRepository)
+
+    const input = MockCustomerModel()
+    input.name = ""
+
+    await expect(useCase.execute(input)).rejects.toThrow("Name is required")
+  })
+
+  it('should throw an error when customer address street is missing', async () => {
+    const customerRepository = MockRepository()
+    const useCase = new CreateCustomerUseCase(customerRepository)
+
+    const input = MockCustomerModel()
+    input.address.street = ""
+
+    await expect(useCase.execute(input)).rejects.toThrow("Street is required")
+  })
+
+  it('should throw an error when customer address number is missing', async () => {
+    const customerRepository = MockRepository()
+    const useCase = new CreateCustomerUseCase(customerRepository)
+
+    const input = MockCustomerModel()
+    input.address.number = 0
+
+    await expect(useCase.execute(input)).rejects.toThrow("Number is required")
+  })
+
+  it('should throw an error when customer address zip is missing', async () => {
+    const customerRepository = MockRepository()
+    const useCase = new CreateCustomerUseCase(customerRepository)
+
+    const input = MockCustomerModel()
+    input.address.zip = ""
+
+    await expect(useCase.execute(input)).rejects.toThrow("Zip is required")
+  })
+
+  it('should throw an error when customer address city is missing', async () => {
+    const customerRepository = MockRepository()
+    const useCase = new CreateCustomerUseCase(customerRepository)
+
+    const input = MockCustomerModel()
+    input.address.city = ""
+
+    await expect(useCase.execute(input)).rejects.toThrow("City is required")
+  })
 })
