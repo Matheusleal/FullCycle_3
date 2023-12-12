15c58a28d67e6143d4771482ad5f3e41d1180788
Matheus Leal <theusleals@outlook.com>
2023-12-12 18:31:42 +0000

    Preparando teste de criação de Customer
    + Correção em nome de arquivo de teste de unidade find.customer

---
+++ Clean_Architecture/src/use_case/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,42 @@
+import Customer from "../../../domain/customer/entity/customer"
+import Address from "../../../domain/customer/entity/value-object/address"
+
+const MockCustomerModel = () => {
+  const customer = {
+    name: "John Doe",
+    address: {
+      street: "Street 1",
+      city: "City 1",
+      number: 1,
+      zip: "Zipcode 1",
+    }
+  }
+  return customer
+}
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  }
+}
+
+describe('Unit Test create Customer Use Case', () => {
+
+  it('should create a customer', async () => {
+    const customerRepository = MockRepository()
+    const useCase = new CreateCustomerUseCase(customerRepository)
+
+    const input = MockCustomerModel()
+    const output = await useCase.execute(input)
+
+    const ExpectedOutput = {
+      id: expect.any(String),
+      ...input,
+    }
+
+    expect(output).toEqual(ExpectedOutput)
+  })
+})
