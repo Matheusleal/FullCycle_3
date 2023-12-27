4844057a78c4516331eb52e8c96a3d6a954579f7
Matheus Leal <theusleals@outlook.com>
2023-12-27 19:20:08 +0000

    Criando primeiro teste E2E

---
+++ Clean_Architecture/src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,34 @@
+import {app, sequelize} from "../express"
+import request from "supertest"
+
+describe("E2E test for customer", () => {
+
+  beforeEach(async () => {
+    await sequelize.sync({force: true})
+  })
+
+  afterAll(async () => {
+    await sequelize.close()
+  })
+
+  it("should create a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street 1",
+          number: 123,
+          zip: "12345",
+          city: "City"
+        }
+      })
+
+    expect(response.status).toBe(200)
+    expect(response.body.name).toBe("John")
+    expect(response.body.address.street).toBe("Street 1")
+    expect(response.body.address.number).toBe(123)
+    expect(response.body.address.zip).toBe("12345")
+    expect(response.body.address.city).toBe("City")
+  })
+})
